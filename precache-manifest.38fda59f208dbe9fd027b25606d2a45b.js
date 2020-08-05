1a7af8969776035fd31fbe08ab9dfd34a5f8ab64
Jéssica Paredes Luna <jeccamilaparedes2001@gmail.com>
2020-08-05 15:42:45 -0500

    Updates

---
+++ precache-manifest.38fda59f208dbe9fd027b25606d2a45b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cd7e5864d2c7c65d5c100df8ee5df06",
+    "url": "/my-app-react2/index.html"
+  },
+  {
+    "revision": "dadd84b6621379e45741",
+    "url": "/my-app-react2/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f97528078f08d8689be1",
+    "url": "/my-app-react2/static/js/2.2ebf5461.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app-react2/static/js/2.2ebf5461.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dadd84b6621379e45741",
+    "url": "/my-app-react2/static/js/main.03cc5d24.chunk.js"
+  },
+  {
+    "revision": "41e004f92856bafc796f",
+    "url": "/my-app-react2/static/js/runtime-main.768798c1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app-react2/static/media/logo.5d5d9eef.svg"
+  }
+]);
